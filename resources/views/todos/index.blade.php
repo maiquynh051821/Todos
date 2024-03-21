77982cfc7f6f68fa4d62638346157344261415cc
maiquynh051821 <102955491+maiquynh051821@users.noreply.github.com>
2024-03-21 16:41:04 +0700

    bootstrap

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -4,19 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Todos</title>
 </head>
 <body>
-    <h1>Todo Page</h1>
-    <ul>
-         @foreach($todos as $todo)
-  
-        <li>
-            {{$todo->name}}
-        </li>
-   
-   
-    @endforeach
-</ul>
+    <div class="container">
+        <h1 class="text-center my-5">Todo Page</h1>
+    <div class="row justify-content-center">
+        <div class="col-md-8 offset-md-2">
+            <div class="card card-default">
+                <div class="card-header">
+                    Todos
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        @foreach($todos as $todo)
+                 
+                       <li class="list-group-item">
+                           {{$todo->name}}
+                       <button class="btn btn-primary btn-sm float-end">Chi tiet</button>
+                    </li> 
+                   @endforeach
+               </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
 </body>
 </html>
