964e41de3e1146d221fd97a71d5579a2364e93bf
maiquynh051821 <102955491+maiquynh051821@users.noreply.github.com>
2024-03-25 16:11:17 +0700

    Done

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command(Emaildaily::class)->everyMinute();
+         $schedule->command(Emaildaily::class)->everyFiveMinutes();;
     }
 
     /**
